e554c182a8e7d11ca898f08e220acecca467d895
Ardilan13 <dilan2191976@correo.uis.edu.co>
2021-10-24 10:14:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/primeraapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.primeraapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
